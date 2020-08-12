b08b5177266a27435bc32ab5bb3ea1207c647fa8
Plamen Ivanov <plamen.ivanov@acronis.com>
2020-08-12 14:57:42 +0300

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="home">
+		<h2>Artistas Todo Frontend</h2>
+		<img alt="Vue logo" src="../assets/logo.png">
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Home'
+	};
+</script>
